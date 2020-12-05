8e2dfd9aaebeaf5cc670cfbd93ca9fb6b66b3d11
Irina <irensaywhen@gmail.com>
2020-12-05 11:20:49 +0300

    basic authorization for genius

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 
 class Auth extends Component {
   render() {
-    return <div>Authentication</div>;
+    const clientId =
+      'HP1jLDsRur3BO0mSzS5BJv7SfHVgN-xtLI7XxUSkkclRjiC10C-fvosZ0zmUGVYD';
+    const redirectUri = '/';
+    const scope = 'me create_annotation manage_annotation vote';
+    const state = 'foobar';
+    const responseType = 'token';
+    return (
+      <div>
+        <a
+          href={`https://api.genius.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&state=${state}&response_type=${responseType}`}
+          className='text-white'
+        >
+          Authenticate
+        </a>
+      </div>
+    );
   }
 }
 
