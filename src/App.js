445a78a78ebc7739cdf32c5988c4cc81384a170c
Irina <irensaywhen@gmail.com>
2020-12-05 10:57:21 +0300

    Basic routing added to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import Influences from './containers/Influences/Influences';
-import Layout from './containers/Layout/Layout';
+import React, { Component } from 'react';
+import {
+  Switch,
+  Route,
+  BrowserRouter as Router,
+  //Redirect,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className='font-sans bg-secondary min-h-screen'>
-      <Layout>
-        <Influences />
-      </Layout>
-    </div>
-  );
+import Layout from './containers/Layout/Layout';
+// Think about renaming influences to features
+import Influences from './containers/Influences/Influences';
+import Auth from './containers/Auth/Auth';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='font-sans bg-secondary min-h-screen'>
+          <Layout>
+            <Switch>
+              <Route path='/feat' exact component={Influences} />
+              <Route path='/' exact component={Auth} />
+            </Switch>
+          </Layout>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
